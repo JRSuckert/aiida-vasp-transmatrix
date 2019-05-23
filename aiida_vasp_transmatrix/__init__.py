88347a7a8a989dedf0cd5b91d899a3dd0aef5f03
Jens Rene Suckert <jens.suckert@gmail.com>
2019-05-23 15:39:05 +0200

    initial commit

---
+++ aiida_vasp_transmatrix/__init__.py
@@ -0,0 +1,9 @@
+"""
+aiida_vasp_transmatrix
+
+Calculation/Parser plugin postprocessing outputs obtained from VASP 5.3.3 CR_MOD
+"""
+
+from __future__ import absolute_import
+
+__version__ = "0.1.0a0"
